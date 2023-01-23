dffe84ab3169fb2cd00c4a6a3a6891cf6be396c0
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-23 13:59:05 +0900

    Update plate_generator.py

--- plate_generator.py
+++ plate_generator.py
@@ -20,7 +20,7 @@ class PlateGenerator:
         self.Char1_g, self.char_list_g = load("./char1_g/")
         self.Region_g, self.region_list_g = load("./region_g/")
 
-    def Generation(self, plate, save, plate_type):
+    def Generation(self, plate, save, plate_type, region_name=None):
         
         assert plate_type in ["short", "long", "yellow", "old", "green"], "Please choose the correct the plate type"
         
@@ -28,7 +28,7 @@ class PlateGenerator:
             generate_plate(plate_path="plate.jpg", 
                        plate=plate, num_size=(56, 83), num_size_2=None, 
                        num_list=self.number_list, init_size=(13, 36), # start from left to right
-                       char_list=self.char_list, regions=None,
+                       char_list=self.char_list, regions=None, three_digit = None,
                        num_ims=self.Number, char_size=(60, 83), region_name=None,
                        char_ims=self.Char1, label_prefix=plate_type,
                        save_path=self.save_path, region_size=None,
@@ -38,7 +38,7 @@ class PlateGenerator:
             generate_plate(plate_path="plate.jpg", 
                        plate=plate, num_size=(40, 83), num_size_2=None, 
                        num_list=self.number_list, init_size=(46, 10),
-                       char_list=self.char_list, regions=None,
+                       char_list=self.char_list, regions=None, three_digit = None,
                        num_ims=self.Number, char_size=(49, 70),  region_name=None,
                        char_ims=self.Char1, label_prefix=plate_type,
                        save_path=self.save_path, region_size=None,
@@ -46,25 +46,27 @@ class PlateGenerator:
         
             
         elif plate_type == "yellow":
+            assert region_name != None, "Please insert a region name"
             generate_plate(plate_path="plate_y.jpg", 
                        plate=plate, num_size=(44, 60), num_size_2=(64, 90), 
                        num_list=self.number_list_y, char_list=self.char_list_y,
                        num_ims=self.Number_y, char_ims=self.Char1_y,
                        init_size=(8, 76), # start from left to right
-                       regions=self.Region_y,
-                       char_size=(64, 62), region_name="서울",
+                       regions=self.Region_y, three_digit = None,
+                       char_size=(64, 62), region_name=region_name,
                        label_prefix=plate_type,
                        save_path=self.save_path, region_size=(88, 60),
                        save_=save, plate_size=(336, 170))
             
         elif plate_type == "old":
+            assert region_name != None, "Please insert a region name"
             generate_plate(plate_path="plate_g.jpg", 
                        plate=plate, num_size=(44, 60), num_size_2=(64, 90), 
                        num_list=self.number_list_g, char_list=self.char_list_g,
                        num_ims=self.Number_g, char_ims=self.Char1_g,
                        init_size=(8, 76), # start from left to right
-                       regions=self.Region_g, 
-                       char_size=(64, 62), region_name="서울",
+                       regions=self.Region_g, three_digit = None,
+                       char_size=(64, 62), region_name=region_name,
                        label_prefix=plate_type,
                        save_path=self.save_path, region_size=(88, 60),
                        save_=save, plate_size=(336, 170))
@@ -76,5 +78,5 @@ class PlateGenerator:
                        num_ims=self.Number_g, char_ims=self.Char1_g, region_size=None,
                        init_size=(8, 78), # start from left to right
                        char_size=(60, 65), label_prefix=plate_type, regions=None,
-                       save_path=self.save_path, region_name=None,
+                       save_path=self.save_path, region_name=None, three_digit = None,
                        save_=save, plate_size=(336, 170))

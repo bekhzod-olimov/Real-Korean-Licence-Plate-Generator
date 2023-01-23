b22363b2747b20828ab9ff6e3f29eb27d68875ca
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-23 16:20:23 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,36 +4,38 @@ import pandas as pd
 
     
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--img_dir", help="save image directory",
+parser.add_argument("-dp", "--data_path", help="Path to the csv file",
+                    type=str, default="test.csv")
+parser.add_argument("-sp", "--save_path", help="save image directory",
                     type=str, default="./new_samples/to_test_new/")
 parser.add_argument("-n", "--num", help="number of image",
                     type=int, default=3)
 parser.add_argument("-s", "--save", help="save or imshow",
                     type=bool, default=True)
+parser.add_argument("-r", "--random", help="Random plate numbers",
+                    type=bool, default=True)
 
 args = parser.parse_args()
-img_dir = args.img_dir
-generator = PlateGenerator(img_dir)
-
-# num_img = args.num
+save_path = args.save_path
+data_path = args.data_path
+random = args.random
 Save = args.save
+sample = "100마0000"
+sample = "서울17마0000"
 
-# df = pd.read_csv("/home/ubuntu/workspace/bekhzod/imagen/lp_recognition_cropped/train/labels.csv")
-df = pd.read_csv("test.csv")
-texts = []
-for i, name in (enumerate(df['filename'])):
-    plate_num = os.path.splitext(name)[0]
-    texts.append(plate_num)
-        
-for idx, im_path in enumerate(texts):
-    if len(im_path) < 8:
-        generator.Generation(im_path, save=Save, plate_type="long")
-    elif len(im_path) > 8:
-        split_ = os.path.splitext(os.path.basename(im_path))[0]
+if random:
+    generator = PlateGenerator(save_path=save_path, random=random)
+    if len(sample) > 8:
+        split_ = os.path.splitext(os.path.basename(sample))[0]
         region_name = split_[:2]
         digits = split_[2:]
-        generator.Generation(digits, save=Save, plate_type="yellow", region_name=region_name)
+        generator.Generation(digits, save=Save, num=10, plate_type="yellow", region_name=region_name)
+    else:
+        generator.Generation(sample, save=Save, num=10, plate_type="long")
+    
+
+else:
+    generator = PlateGenerator(save_path=save_path, random=random)
 
 
-
-        
+    

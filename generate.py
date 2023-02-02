f228b2d4c0928604e744fee0854bb8d45a73e304
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-02-02 10:47:28 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("-sp", "--save_path", help = "Directory to save generated images", type = str, default = "./new_samples/aaa/")
     parser.add_argument("-s", "--save", help = "Saving option", type = bool, default = True)
     parser.add_argument("-np", "--number_of_plates", help = "Number of images to generate", type = int, default = 10)
-    parser.add_argument("-r", "--random", help = "Generate random plate numbers", type = bool, default = False)
+    parser.add_argument("-r", "--random", help = "Generate random plate numbers", type = bool, default = True)
     
     
     args = parser.parse_args()
